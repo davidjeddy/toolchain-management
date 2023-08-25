e55566ba9163d631396885cfc88dca285a072d9c
David Eddy <david.eddy@ingenico.com>
2023-08-25 17:05:28 +0200

    0.20.0 FIXED CHANGELOG to better align with the auto-taggin-publishing process coming soon. UPDATED pre-commit related ./libs/bash

--- libs/bash/pre_commit.sh
+++ libs/bash/pre_commit.sh
@@ -21,7 +21,16 @@ printf "INFO: PROJECT_ROOT value is \n%s\n" "$PROJECT_ROOT"
 
 # get a list of changed files when using only the git staged list against previouse commit
 declare TF_FILES_CHANGED
-TF_FILES_CHANGED=$(git diff main --name-only | grep tf\$ | grep -v examples/ | grep -v .tmp/ | grep -v libs/kics || true)
+TF_FILES_CHANGED=$(git diff HEAD~1 --name-only | grep tf\$ | \
+    grep -v .tmp/ | \
+    grep -v examples/ | \
+    grep -v libs | \
+    grep -v README.md | \
+    grep -v sbom.xml | \
+    grep -v terraform.tf \
+    || true \
+)
+
 export TF_FILES_CHANGED
 printf "INFO: TF_FILES_CHANGED value is \n%s\n" "$TF_FILES_CHANGED"
 
@@ -34,7 +43,7 @@ fi
 declare MODULES_DIR
 if [[ $TF_FILES_CHANGED != "" ]]
 then
-    MODULES_DIR=$(echo "$TF_FILES_CHANGED" | xargs -L1 dirname | uniq)
+    MODULES_DIR=$(echo "$TF_FILES_CHANGED" | xargs -L1 dirname | sort | uniq)
     export MODULES_DIR
     printf "INFO: MODULES_DIR value is \n%s\n" "$MODULES_DIR"
 fi
@@ -42,25 +51,29 @@ fi
 ## functions
 
 # shellcheck disable=1091
-. "${PROJECT_ROOT}/libs/bash/pre_commit_functions.sh"
+source "${PROJECT_ROOT}/libs/bash/pre_commit_functions.sh"
 
 ## logic
 
 for DIR in $MODULES_DIR
 do
     printf "INFO: Reset to project home directory.\n"
-    cd "${PROJECT_ROOT}" || exit
+    cd "${PROJECT_ROOT}" || exit 10
 
     printf "INFO: Changing into %s dir if it still exists.\n" "${DIR}"
     cd "$DIR" || continue
 
-    # If a lock file exists, the module was init'd atleast once. Thus we want do it again if the .terraform dir is missing
+    # If a lock file exists and a cache dir does not, The module needs to be initilized.
     if [[ -f ".terraform.lock.hcl" && ! -d ".terraform" ]]
     then
         printf "INFO: Detected this module has not be initilized, doing it for you now.\n"
-        terraform init -no-color -upgrade
-        terraform providers lock -platform=linux_amd64 # ensure providers for Jenkins worker node
+        terraform init -no-color
+        # ensure providers for Jenkins worker node
+        terraform providers lock -platform=linux_amd64
     fi
+
+    # Create tmp dir to hold artifacts and reports
+    createTmpDir
 
     # Do not allow in-project shared modules
     doNotAllowSharedModulesInsideDeploymentProjects
@@ -82,4 +95,4 @@ done
 
 cd "$ORIG_PWD" || exit
 
-printf "INFO: Git pre-commit hook completed.\n"
+printf "INFO: Git pre-commit hook completed successfully.\n"
--- libs/bash/pre_commit_functions.sh
+++ libs/bash/pre_commit_functions.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -e
 
+# TODO use `parallel` to execute functions at the same time to reduce wait time
+# TODO use tool configuration file if present should not require an if/then eval
+
+# Make tmp dir to hold artifacts and reports per module
+function createTmpDir() {
+    if [[ ! -d ".tmp" ]]
+    then
+        mkdir -p ".tmp"
+    fi
+}
+
 function doNotAllowSharedModulesInsideDeploymentProjects() {
     printf "INFO: Do not allow shared modules inside a deployment project.\n"
 
     #shellcheck disable=SC2002 # We do want to cat the file contents and pipeline into jq
+    if [[ ! -f ".terraform/modules/modules.json" ]]
+    then
+        return 0
+    fi
+
+    # shellcheck disable=SC2002
     MODULES_IN_USE=$(cat ".terraform/modules/modules.json" | jq '.Modules[] | .Source')
 
     for MODULE in $MODULES_IN_USE
@@ -42,40 +59,168 @@ function documentation() {
 }
 
 function generateSBOM() {
-    printf "INFO: generating sbom.\n"
-    checkov --directory . --output cyclonedx  > sbom.xml
-    git add sbom.xml || true
+    printf "INFO: generating sbom using checkov (Ignore warning about 'Failed to download module', this is due to a limitation of checkov.)...\n"
+    {
+        if [[ -f "checkov.yml" ]]
+        then
+            # use configuration file if present. Created due to terraform/deployments/terraform/aws/worldline-gc-keycloak-dev/eu-west-1/keycloak/iohd being created BEFORE complaince was mandatory    
+            checkov \
+                --config-file checkov.yml \
+                --directory . \
+                --output cyclonedx \
+                > sbom.xml
+        else
+            checkov \
+                --directory . \
+                --output cyclonedx \
+                > sbom.xml
+        fi
+        git add sbom.xml || true
+    } || {
+        echo "ERR: checkov SBOM failed to generate."
+        exit 1
+    }
 }
 
-# TODO use `parallel` to execute compliance tools at the same time to reduce wait time
 function terraformCompliance() {
     printf "INFO: checkov (Ignore warning about 'Failed to download module', this is due to a limitation of checkov.)...\n"
-    checkov \
-    --directory . \
-    --download-external-modules false \
-    --framework terraform \
-    --quiet \
-    --skip-path .tmp/ \
-    --skip-path examples/ \
-    --skip-path libs/
+    {
+        if [[ -f "checkov.yml" ]]
+        then
+            # use configuration file if present.
+            checkov \
+                --config-file checkov.yml \
+                --directory . \
+                --download-external-modules false \
+                --framework terraform \
+                --output junitxml \
+                --quiet \
+                --skip-path .terra*/ \
+                --skip-path .tmp/ \
+                --skip-path examples/ \
+                --skip-path libs/ \
+                > "$(pwd)/.tmp/junit-checkov.xml"
+        else
+            # 'normal' full scan
+            checkov \
+                --directory . \
+                --download-external-modules false \
+                --framework terraform \
+                --quiet \
+                --skip-path .terra*/ \
+                --skip-path .tmp/ \
+                --skip-path examples/ \
+                --output junitxml \
+                --skip-path libs/ \
+                > "$(pwd)/.tmp/junit-checkov.xml"
+        fi
+    } || {
+        echo "ERR: checkov failed. Check report saved to $(pwd)/.tmp/junit-checkov.xml"
+        exit 1
+    }
 
-    # Temp disabled due to two bugs with terrascan
-    # https://github.com/tenable/terrascan/issues/1262 and https://github.com/tenable/terrascan/issues/1266
+    # # Temp disabled due to not yet supporting TF_TOKEN_* auth source
+    # # https://github.com/tenable/terrascan/issues/1566
+    # # Also having problems with using GitLab PAT in Jenkins
     # printf "INFO: terrascan...\n"
-    # terrascan scan --iac-type terraform --non-recursive
+    # {
+    #     if [[ -f "terrascan.toml" ]]
+    #     then
+    #         # use configuration file if present.
+    #         terrascan scan \
+    #             --config-path terrascan.tml \
+    #             --iac-type terraform \
+    #             --log-level error \
+    #             --non-recursive \
+    #             --output junit-xml \
+    #             --use-colors f \
+    #             > .tmp/junit-terrascan.xml
+    #     else
+    #         terrascan scan \
+    #             --iac-type terraform \
+    #             --log-level error \
+    #             --non-recursive \
+    #             --output junit-xml \
+    #             --use-colors f \
+    #             > .tmp/junit-terrascan.xml
+    #     fi
+    # } || {
+    #     echo "ERR: terrascan failed. Check Junit reports in .tmp"
+    #     exit 1
+    # }
 
-    printf "INFO: trivy...\n"
-    trivy . --concise-output --exclude-downloaded-modules --no-color
+    {
+        if [[ -f "tfsec.yml" ]]
+        then
+            # use configuration file if present.
+            tfsec . \
+                --concise-output \
+                --config-file tfsec.yml \
+                --exclude-downloaded-modules \
+                --exclude-path "examples,.terra*,.tmp" \
+                --format junit \
+                --no-color \
+                --no-module-downloads \
+                > "$(pwd)/.tmp/junit-tfsec.xml"                
+        else
+            tfsec . \
+                --concise-output \
+                --exclude-downloaded-modules \
+                --exclude-path "examples,.terra*,.tmp" \
+                --format junit \
+                --no-color \
+                --no-module-downloads \
+                > "$(pwd)/.tmp/junit-tfsec.xml"
+        fi
+    } || {
+        echo "ERR: tfsec failed. Check report saved to $(pwd)/.tmp/junit-tfsec.xml"
+        exit 1
+    }
 
-    printf "INFO: kics (Takes 15 to 30 seconds, please wait)...\n"
-    kics scan \
-        --no-progress \
-        --cloud-provider "aws" \
-        --exclude-paths "*" \
-        --no-color \
-        --path "./" \
-        --queries-path "$PROJECT_ROOT/.tmp/toolchain-management/libs/kics/assets/queries/terraform/aws" \
-        --type "Terraform"
+    {
+        printf "INFO: kics (Takes 15 to 30 seconds, please wait)...\n"
+        
+        declare KICS_IGNORE_RULE_IDS
+        if [[ ! -f ".terraform.lock.hcl" && ! -d ".terraform" ]]
+        then
+            # if shared module, add ignore rules
+            export KICS_IGNORE_RULE_IDS="e38a8e0a-b88b-4902-b3fe-b0fcb17d5c10"
+        fi
+
+        # use configuration file if present.
+        if [[ -f "kics.yml" ]]
+        then
+            kics scan \
+                --cloud-provider "aws" \
+                --config "kics.yml" \
+                --exclude-paths "*" \
+                --exclude-queries "$KICS_IGNORE_RULE_IDS" \
+                --no-color \
+                --no-progress \
+                --output-name "junit-kics" \
+                --output-path "$(pwd)/.tmp" \
+                --path "./" \
+                --queries-path "$PROJECT_ROOT/.tmp/toolchain-management/libs/kics/assets/queries/terraform/aws" \
+                --report-formats "junit" \
+                --type "Terraform"
+        else
+            kics scan \
+                --cloud-provider "aws" \
+                --exclude-paths "*" \
+                --exclude-queries "$KICS_IGNORE_RULE_IDS" \
+                --no-color \
+                --no-progress \
+                --output-name "junit-kics" \
+                --output-path "$(pwd)/.tmp" \
+                --path "./" \
+                --queries-path "$PROJECT_ROOT/.tmp/toolchain-management/libs/kics/assets/queries/terraform/aws" \
+                --report-formats "junit" \
+                --type "Terraform"
+        fi
+    } || {
+        echo "ERR: kics failed. Check report saved to $(pwd)/.tmp/junit-kics.xml"
+        exit 1
+    }
 }
 
 function terraformLinting() {
@@ -84,5 +229,5 @@ function terraformLinting() {
     printf "INFO: Terraform/Terragrunt fmt and linting...\n"
     terraform fmt -no-color -recursive . 
     terragrunt hclfmt .
-    tflint --no-color .
+    tflint --chdir="$(pwd)" --fix --module --no-color
 }
