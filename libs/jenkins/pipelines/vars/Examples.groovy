7b870354ef843485723dec844eaaaa45fc29cc55
David Eddy <david.eddy@worldline.com>
2024-12-16 10:28:37 +0100

    ADD container image building pipeline for AWS ECR hosted images

--- libs/jenkins/pipelines/vars/Examples.groovy
+++ libs/jenkins/pipelines/vars/Examples.groovy
@@ -2,6 +2,7 @@
 
 @Library('toolchain-management')
 
+// IAC Shared Module
 // Module(Number, Number, String, String)
 // Module(ProjectGitLabId, PipelineTimeout, CronSchedule, SlackChannel)
 // Module(3319, 'H 4 * * 1-5', 15, 'nl-pros-centaurus-squad-releases')
@@ -10,5 +11,17 @@ SharedModule(
     78465,
     15,
     'H */6 * * 1-5',
+    'nl-pros-centaurus-squad-releases'
+)
+
+// Container Image
+// Module(Number, Number, String, String)
+// Module(ProjectGitLabId, PipelineTimeout, CronSchedule, SlackChannel)
+// Module(3319, 'H 4 * * 1-5', 15, 'nl-pros-centaurus-squad-releases')
+// Note The CRON schedule is Jenkins enabled timer. Meaning we can do random times within an hour bound. https://www.jenkins.io/doc/book/pipeline/syntax/
+ContainerImage(
+    84424,
+    30,
+    'H */8 30 * 1-5',
     'nl-pros-centaurus-squad-releases'
 )
